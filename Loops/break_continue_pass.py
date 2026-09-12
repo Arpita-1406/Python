82f3810f072f2629d09fefbc4b7ecdc59b83a80b
Arpita-1406 <pandeyarpita013@gmail.com>
2026-09-13 00:01:07 +0530

    Loops

---
+++ Loops/break_continue_pass.py
@@ -0,0 +1,18 @@
+for i in range(0, 21):
+    print(i)
+    if i == 11:
+        break # Cancel the execution of this loop now
+
+
+for i in range(1, 20):
+    if i == 10:
+        continue # continue the loop for the next iteration here itself
+    print(i)
+
+
+
+i = 3
+
+if i == 32:
+    pass # do nothing
+print("End of program") 
