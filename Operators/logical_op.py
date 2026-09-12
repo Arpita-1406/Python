a5b4c33085a6750def9021e7248ba83685a71c55
Arpita-1406 <pandeyarpita013@gmail.com>
2026-09-12 14:29:40 +0530

    Operators

---
+++ Operators/logical_op.py
@@ -0,0 +1,16 @@
+
+# Logical Operators
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("For Or Operator...")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("Not Operator")
+print(not(True) ) 
+print(not(False) ) 
